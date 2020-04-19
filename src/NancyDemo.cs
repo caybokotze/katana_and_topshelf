7e18b7d1149e7da30e8941d314740a4db89602cb
Caybo Kotze <kotzecabo@gmail.com>
2020-04-19 09:44:21 +0200

    Initial Commit

---
+++ src/NancyDemo.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace AspNetSelfHostDemo
+{
+    public class NancyDemo : NancyModule
+    {
+        public NancyDemo()
+        {
+            Get["/nancy/demo"] = _ => "Go world.";
+        }
+    }
+}
